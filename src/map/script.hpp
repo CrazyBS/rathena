078e0e803cdd18ca9523783dbeb0ea53c48f5940
CrazyBS <bpyl@hotmail.com>
2018-04-16 12:47:29 -0700

    Revert "Updated getunitdata / setunitdata (#3029)"
    This reverts commit d757b59d81380a59a7f541d737811c0161922ee5.

--- src/map/script.hpp
+++ src/map/script.hpp
@@ -470,7 +470,6 @@ enum unitdata_mobtypes {
 	UMOB_AMOTION,
 	UMOB_ADELAY,
 	UMOB_DMOTION,
-	UMOB_TARGETID,
 };
 
 enum unitdata_homuntypes {
@@ -513,7 +512,6 @@ enum unitdata_homuntypes {
 	UHOM_AMOTION,
 	UHOM_ADELAY,
 	UHOM_DMOTION,
-	UHOM_TARGETID,
 };
 
 enum unitdata_pettypes {
@@ -593,7 +591,6 @@ enum unitdata_merctypes {
 	UMER_AMOTION,
 	UMER_ADELAY,
 	UMER_DMOTION,
-	UMER_TARGETID,
 };
 
 enum unitdata_elemtypes {
@@ -635,7 +632,6 @@ enum unitdata_elemtypes {
 	UELE_AMOTION,
 	UELE_ADELAY,
 	UELE_DMOTION,
-	UELE_TARGETID,
 };
 
 enum unitdata_npctypes {
